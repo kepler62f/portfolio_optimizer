7a68a60e81d2f2c1d749ba96a174f4757041ec3c
kepler62f <blue.kepler62f@gmail.com>
2017-08-25 16:23:27 +0800

    initial

--- create_portfolio/views.py
+++ create_portfolio/views.py
@@ -20,15 +20,41 @@ def index(request):
     section("Covariance matrix")
     print(cov_mat)
 
+    # have to keep target within domain of expected returns of assets
+    # else cvxopt/numpy will return domain error or convergence problem
+    target_ret = avg_rets.quantile(0.7)
+    weights = optimizer.markowitz_portfolio(cov_mat, avg_rets, 0.0049, allow_short=False, market_neutral=False)
+
+    print_portfolio_info(returns, avg_rets, weights)
+
+
     context = {
-        'avg_rets': avg_rets
+        'avg_rets': avg_rets,
+        'cov_mat': cov_mat,
+        'weights': weights
     }
 
     return render(request, 'create_portfolio/base.html', context)
 
 
 
+
+##################################
+### Auxiliary Functions
+##################################
+
 def section(caption):
     print('\n\n' + str(caption))
     print('-' * len(caption))
 
+def print_portfolio_info(returns, avg_rets, weights):
+    """
+    Print information on expected portfolio performance.
+    """
+    ret = (weights * avg_rets).sum()
+    std = (weights * returns).sum(1).std()
+    sharpe = ret / std
+    print("Optimal weights:\n{}\n".format(weights))
+    print("Expected return:   {}".format(ret))
+    print("Expected variance: {}".format(std**2))
+    print("Expected Sharpe:   {}".format(sharpe))
