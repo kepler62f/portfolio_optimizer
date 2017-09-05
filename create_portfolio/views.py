fb8b31d688134354574011f189d62ec7ed155802
kepler62f <blue.kepler62f@gmail.com>
2017-09-05 16:18:55 +0800

    enabled merging asset classes into one dataframe

--- create_portfolio/views.py
+++ create_portfolio/views.py
@@ -45,13 +45,21 @@ class OptimizerView(APIView):
         bonds_df = pd.DataFrame.from_records(bonds)
         gold_df = pd.DataFrame.from_records(gold)
 
-        # set date as dataframem index
+        # set date as dataframe index
         cash_ts = cash_df.set_index("date")
         sp500_ts = sp500_df.set_index("date")
         europe_ts = msci_europe_df.set_index("date")
         em_ts = msci_em_df.set_index("date")
         bonds_ts = bonds_df.set_index("date")
         gold_ts = gold_df.set_index("date")
+
+
+        cash_ts.columns = ["Cash"]
+        sp500_ts.columns = ["S&P 500"]
+        europe_ts.columns = ["MSCI Europe"]
+        em_ts.columns = ["MSCI Emerging Markets"]
+        bonds_ts.columns = ["ICE US Core Bond"]
+        gold_ts.columns = ["Gold"]
 
         # resample daily time series as monthly
         # fill NA holes with next valid observation
@@ -64,41 +72,69 @@ class OptimizerView(APIView):
         mth_gold_ts = gold_ts.asfreq('M').fillna(method="backfill")
 
         # filter into 5-year (60-mth) monthly return series ending Jun 30, 2017
-        cash_returns = mth_cash_ts["2011-12-31":"2017-06-30"]
+        cash_returns = mth_cash_ts["2012-06-30":"2017-06-30"]
         cash_returns.drop(cash_returns.index[0], inplace=True)
-        sp500_returns = mth_sp500_ts["2011-12-31":"2017-06-30"].pct_change()
+        sp500_returns = mth_sp500_ts["2012-06-30":"2017-06-30"].pct_change()
         sp500_returns.drop(sp500_returns.index[0], inplace=True)
-        europe_returns = mth_europe_ts["2011-12-31":"2017-06-30"].pct_change()
+        europe_returns = mth_europe_ts["2012-06-30":"2017-06-30"].pct_change()
         europe_returns.drop(europe_returns.index[0], inplace=True)
-        em_returns = mth_em_ts["2011-12-31":"2017-06-30"].pct_change()
+        em_returns = mth_em_ts["2012-06-30":"2017-06-30"].pct_change()
         em_returns.drop(em_returns.index[0], inplace=True)
-        bonds_returns = mth_bonds_ts["2011-12-31":"2017-06-30"].pct_change()
+        bonds_returns = mth_bonds_ts["2012-06-30":"2017-06-30"].pct_change()
         bonds_returns.drop(bonds_returns.index[0], inplace=True)
-        gold_returns = mth_gold_ts["2011-12-31":"2017-06-30"].pct_change()
+        gold_returns = mth_gold_ts["2012-06-30":"2017-06-30"].pct_change()
         gold_returns.drop(gold_returns.index[0], inplace=True)
 
-        # data = []
-        # dates = []
-        # assets = []
-        #
+        frames = []
+
+        # print(cash_returns)
+        # print(cash_returns.loc['instrument_value'])
+
+        # data.append(cash_returns.loc["instrument_value"])
+        # data.append(sp500_returns.loc["instrument_value"])
+        # print(data)
+
+        # merge = pd.concat([cash_returns, gold_returns], axis=1)
+        # print(merge)
+
+        assets = []
+        dates = pd.date_range('7/1/2012', periods=60, freq='M', tz='UTC')
+        # print(dates)
 
         for key, value in request.GET.items():
             if (key == "addedCash") and (value == "true"):
-                print("add cash")
+                # assets.append("Cash")
+                # print(assets)
+                frames.append(cash_returns)
             if (key == "addedSP500") and (value == "true"):
-                print("add sp500")
+                # assets.append("S&P 500")
+                # print(assets)
+                frames.append(sp500_returns)
             if (key == "addedEurope") and (value == "true"):
-                print("add Europe")
+                # assets.append("Europe")
+                # print(assets)
+                frames.append(europe_returns)
             if (key == "addedEM") and (value == "true"):
-                print("add EM")
+                # assets.append("MSCI Emerging Markets")
+                # print(assets)
+                frames.append(em_returns)
             if (key == "addedBonds") and (value == "true"):
-                print("add Bonds")
+                # assets.append("Bonds")
+                # print(assets)
+                frames.append(bonds_returns)
             if (key == "addedGold") and (value == "true"):
-                print("add Gold")
+                # assets.append("Gold")
+                # print(assets)
+                frames.append(gold_returns)
+
+        if (len(frames) > 0):
+            returns = pd.concat(frames, axis=1)
+            print(returns)
 
 
 
-
+        # avg_rets = returns.mean()
+        # cov_mat = returns.cov()
 
 
 
